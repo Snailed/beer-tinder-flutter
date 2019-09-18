66023e86ace27c1791efe315ce7d0fcc5bf41f67
Snailed <rasmus.hag.lovstad@gmail.com>
2019-09-18 20:50:38 +0200

    Integreret, men ikke testet. Push IKKE TIL MASTER!

--- lib/create-beer-page.dart
+++ lib/create-beer-page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class CreateBeerPage extends StatefulWidget {
   CreateBeerPage({Key key, this.title}) : super(key: key);
 
@@ -18,68 +19,258 @@ class CreateBeerPage extends StatefulWidget {
 }
 
 class _CreateBeerPageState extends State<CreateBeerPage> {
-  int _counter = 0;
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  String beer_type = 'IPA';
+  double slider_val = 5;
+  var list_tekst = ["Start med at vælge din øl-type","Hvor stærk skal øllen være?"];
+  var list_widgets;
+  var index = 0;
+
+  final ListCon = new ScrollController();
+  final nameCon = TextEditingController();
+
+  bool bygVal = false;
+  bool rugVal = false;
+  bool hvedeVal = false;
+
+
+  void addComponent(callId) {
+    if(callId==index) {
+      _listKey.currentState.insertItem(index+1, duration: Duration(milliseconds: 1000));
+      _listKey.currentState.widget.controller.animateTo(_listKey.currentState.widget.controller.position.maxScrollExtent+100, duration: Duration(milliseconds: 800), curve: Curves.easeIn); //Temporary auto- scroll fix
+      index++;
+    }
+
+  }
+
+
+
+  Widget _buildItem([int index]) {
+    return list_widgets[index];
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the CreateBeerPage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text('Hygge alligevel'),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    list_widgets = [
+      Container(
+        margin: const EdgeInsets.only(left: 20.0, right: 20.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            SizedBox(height: 60),
             Text(
-              'You have pushed the button this many times:',
+              "Lad os starte med at vælge en øl-type",
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.title,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            DropdownButton<String>(
+              value: beer_type,
+              iconSize: 24,
+              elevation: 16,
+              style: TextStyle(
+                  color: Colors.deepPurple
+              ),
+              underline: Container(
+                height: 2,
+                color: Colors.deepPurpleAccent,
+              ),
+              onChanged: (String newValue) {
+                setState(() {
+                  beer_type = newValue;
+                });
+                addComponent(0);
+              },
+              items: <String>['IPA', 'Pilsner', 'Brown Ale', 'Lager']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              })
+                  .toList(),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+
+      Container(
+        margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 60),
+            Text(
+              "Godt valg! Hvor stærk skal din øl være?",
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.title,
+            ),
+            Text(
+              slider_val.round().toString(),
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.display1,
+            ),
+
+            Slider(
+              value: slider_val,
+              onChanged: (newRating) {
+                setState(() => slider_val = newRating);
+              },
+              onChangeEnd: (event){
+                addComponent(1);
+              },
+              min: 0,
+              max: 10,
+              divisions: 10,
+            ),
+
+          ],
+        ),
+      ),
+
+      Container(
+        margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 60),
+            Text(
+              "Hvad skal øllen være lavet på?",
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.title,
+            ),Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                // [Monday] checkbox
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Byg"),
+                    Checkbox(
+                      value: bygVal,
+                      onChanged: (bool value) {
+                        setState(() {
+                          bygVal = value;
+                          addComponent(2);
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                // [Tuesday] checkbox
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Rug"),
+                    Checkbox(
+                      value: rugVal,
+                      onChanged: (bool value) {
+                        setState(() {
+                          rugVal = value;
+                          addComponent(2);
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                // [Wednesday] checkbox
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Hvede"),
+                    Checkbox(
+                      value: hvedeVal,
+                      onChanged: (bool value) {
+                        setState(() {
+                          hvedeVal = value;
+                          addComponent(2);
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+
+      Container(
+        margin: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 30.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 60), Text(
+              "Hvad skal mesterværket hedde?",
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.title,
+            ),
+            TextField(
+              controller: nameCon,
+              decoration: InputDecoration(labelText: 'Navn'),
+              onSubmitted: (event){
+                setState(() {});
+                addComponent(3);
+              },
+            ),
+          ],
+        ),
+      ),
+
+      Container(
+        margin: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 30.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 60), Text(
+              "Beskriv smagen af "+nameCon.text,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.title,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Beskrivelse'),
+
+              onChanged: (event){
+                addComponent(4);
+              },
+            )
+          ],
+        ),
+      ),
+      Container(
+        margin: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 50.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 10),
+            FlatButton(
+                color: Colors.green,
+                onPressed: (){
+
+                },
+                child: Text(
+                  "Opret",
+                  style: TextStyle(color: Colors.white, fontSize: 30),
+                )
+            )
+          ],
+        ),
+      ),
+    ];
+
+
+    return SafeArea(
+        child: AnimatedList(
+          controller: ListCon,
+          key: _listKey,
+          initialItemCount: 1,
+          itemBuilder: (BuildContext context, int index, Animation animation) {
+            return FadeTransition(
+              opacity: animation,
+              child: _buildItem(index),
+            );
+          },
+        ),
+      );
   }
 }
